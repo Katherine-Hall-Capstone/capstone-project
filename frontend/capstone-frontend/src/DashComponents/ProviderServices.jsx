c80e1dd69e48d9705b151ac6cf3b331e173d2391
Katherine Hall <khall206@meta.com>
2025-07-22 18:49:07 -0700

    service-details page

--- frontend/capstone-frontend/src/DashComponents/ProviderServices.jsx
+++ frontend/capstone-frontend/src/DashComponents/ProviderServices.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { FaTrashAlt } from "react-icons/fa"
 
 function ProviderServices({ providerId }) {
     const [services, setServices] = useState([])
@@ -77,42 +78,68 @@ function ProviderServices({ providerId }) {
     }
 
     return (
-        <div className="service-container">
-            <h3>Manage Services</h3>
-            <form onSubmit={handleAddService}>
-                <input
-                    type="text"
-                    placeholder="Service Name"
-                    value={serviceName}
-                    onChange={event => setServiceName(event.target.value)}
-                    required
-                />
-                <input
-                    type="number"
-                    placeholder="Duration (min)"
-                    value={serviceDuration}
-                    onChange={event => setServiceDuration(event.target.value)}
-                    required
-                    min={1}
-                />
-                <textarea
-                    placeholder="Service Details"
-                    value={serviceDetails}
-                    onChange={event => setServiceDetails(event.target.value)}
-                />
-                <button type="submit">Add Service</button>
-            </form>
+        <div>
+            <h3 className="mb-4 text-2xl text-center font-semibold">Manage Services</h3>
 
-            <ul>
-                {services.map(service => (
-                    <li key={service.id}>
-                        {service.name} ({service.duration} min) - {service.details}
-                        <button onClick={() => handleDeleteService(service.id)}>Delete</button>
-                    </li>
-                ))}
-            </ul>
+            <div className="flex justify-around">
+                <div>
+                    <p className="text-xl text-center">Add Service:</p>
+                    <form 
+                        onSubmit={handleAddService}
+                        className="flex flex-col gap-2 mt-3"
+                    >
+                        <input
+                            type="text"
+                            placeholder="Service Name"
+                            value={serviceName}
+                            onChange={event => setServiceName(event.target.value)}
+                            className="p-1 bg-white border border-gray-300 focus:outline-none focus:ring-1 rounded-md"
+                            required
+                        />
+                        <input
+                            type="number"
+                            placeholder="Duration (min)"
+                            value={serviceDuration}
+                            onChange={event => setServiceDuration(event.target.value)}
+                            className="p-1 bg-white border border-gray-300 focus:outline-none focus:ring-1 rounded-md"
+                            required
+                            min={1}
+                        />
+                        <textarea
+                            placeholder="Service Details"
+                            value={serviceDetails}
+                            onChange={event => setServiceDetails(event.target.value)}
+                            className="p-1 h-40 bg-white border border-gray-300 focus:outline-none focus:ring-1 rounded-md resize-none"
+                        />
+                        <button 
+                            type="submit"
+                            className="mt-4 px-3 py-2 bg-slate-900 hover:bg-slate-700 text-white font-semibold rounded-md cursor-pointer"
+                        >
+                            Add Service
+                        </button>
+                    </form>
 
-            {errorMessage && <p className="error-msg">{errorMessage}</p>}
+                    <p className="mt-2 min-h-5 max-w-50 text-red-600 text-center text-sm">{errorMessage}</p>
+                </div>
+
+                <div>
+                    <p className="text-xl text-center">Services</p>
+                    <ul className="mt-3 w-70 space-y-4">
+                        {services.map(service => (
+                            <li key={service.id} className="flex flex-col items-center p-4 bg-slate-500 border border-slate-600 text-white shadow-lg rounded-md">
+                                <div className="font-bold">{service.name} ({service.duration} min):</div> 
+                                <div className="w-full italic break-words text-center text-slate-200">{service.details}</div>
+                                <button 
+                                    onClick={() => handleDeleteService(service.id)}
+                                    className="mt-2 cursor-pointer"
+                                >
+                                    <FaTrashAlt />
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
